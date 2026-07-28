c572bb427455353cd3230369c0bd7b97009481c6
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-07-28 15:45:03 +0300

    feat: server

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module todo-app
+module github.com/inraise/todo-app
 
 go 1.26.2
+
+require go.uber.org/zap v1.28.0
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
---
+++ internal/core/transport/http/server/router.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type ApiVersion string
+
+var (
+	ApiVersion1 = ApiVersion("v1")
+)
+
+type APIVersionRouter struct {
+	*http.ServeMux
+	apiVersion ApiVersion
+}
+
+func NewAPIVersionRouter(apiVersion ApiVersion) *APIVersionRouter {
+	return &APIVersionRouter{
+		ServeMux:   http.NewServeMux(),
+		apiVersion: apiVersion,
+	}
+}
+
+func (r *APIVersionRouter) RegisterRoutes(routes ...Route) {
+	for _, route := range routes {
+		pattern := fmt.Sprintf("%s %s", route.Method, route.Path)
+
+		r.Handle(pattern, route.Handler)
+	}
+}
