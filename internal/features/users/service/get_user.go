3fcfad5d069faa63e6b350465619cd8bfadaf8b3
Daniil Kharitonov <kharitonov.daniil63@gmail.com>
2026-07-29 03:11:40 +0300

    feat: эндпоинт для поиска пользователя по id

---
+++ internal/features/users/service/get_user.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/inraise/todo-app/internal/core/domain"
+)
+
+func (s *UserService) GetUser(
+	ctx context.Context,
+	id int,
+) (domain.User, error) {
+	user, err := s.usersRepository.GetUser(ctx, id)
+	if err != nil {
+		return domain.User{}, fmt.Errorf("get user from repository: %w", err)
+	}
+
+	return user, nil
+}
---
+++ internal/features/users/transport/http/get_user.go
@@ -0,0 +1,41 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/inraise/todo-app/internal/core/logger"
+	"github.com/inraise/todo-app/internal/core/transport/http/response"
+	"github.com/inraise/todo-app/internal/core/transport/http/utils"
+)
+
+type GetUserResponse UserDTOResponse
+
+func (h *UsersHTTPHandler) GetUser(rw http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log := logger.FromContext(ctx)
+	responseHandler := response.NewHTTPResponseHandler(log, rw)
+
+	userId, err := utils.GetIntPathValue(r, "id")
+	if err != nil {
+		responseHandler.ErrorResponse(
+			err,
+			"failed to get userID path value",
+		)
+
+		return
+	}
+
+	user, err := h.usersService.GetUser(ctx, userId)
+	if err != nil {
+		responseHandler.ErrorResponse(
+			err,
+			"failed to get user",
+		)
+
+		return
+	}
+
+	response := GetUserResponse(userDTOFromDomain(user))
+
+	responseHandler.JSONResponse(response, http.StatusOK)
+}
